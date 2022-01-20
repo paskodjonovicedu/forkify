6e47d1cd73d34557bae25fc6dd8df89c5d1182c2
paskodjonovicedu <pasko.djonovic@udg.edu.me>
2022-01-20 12:12:59 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 12;
+export const KEY = '708c08cc-0897-411f-9d9c-a30615966f64';
+export const MODAL_CLOSE_SEC = 2.5;
